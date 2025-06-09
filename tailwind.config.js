71402dd74962b13e5f69f6be910ba0ca9dab4d80
Praatah Gandhi <praatahgandhi@Praatahs-MacBook-Air.local>
2025-06-09 15:26:07 -0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     darkMode: ['class'],
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
   	extend: {
   		animation: {
