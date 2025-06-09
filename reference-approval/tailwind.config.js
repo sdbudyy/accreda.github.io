dff5027f3f1714628cc26a9622e7b1a60a85d7e4
Praatah Gandhi <praatahgandhi@Praatahs-MacBook-Air.local>
2025-06-09 17:01:24 -0600

    Your commit message

--- reference-approval/tailwind.config.js
+++ reference-approval/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
